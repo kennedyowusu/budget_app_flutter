696fbb3c638106428b5548729c0264928be8fbd6
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-03 16:55:26 +0000

    update the custom button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,3 +1,4 @@
+import 'package:budget_app_flutter/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
@@ -11,15 +12,23 @@ class CustomButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onPressed,
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        backgroundColor: MaterialStateProperty.all<Color>(mainColor),
         foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        maximumSize: MaterialStateProperty.all<Size>(Size.fromHeight(50.0)),
+        minimumSize: MaterialStateProperty.all<Size>(Size.fromHeight(50.0)),
+        elevation: MaterialStateProperty.all<double>(0.0),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
         padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
           EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
         ),
       ),
       child: Text(
         text,
-        style: TextStyle(fontSize: 16.0),
+        style: subtitleStyle.copyWith(color: Colors.white),
       ),
     );
   }
