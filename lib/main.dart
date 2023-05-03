9b87b4e564ff947e3a9e64c1971e1c3f6c3d3a7a
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-03 21:41:11 +0000

    use getx in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:budget_app_flutter/view/splash/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const BudgetApp());
@@ -11,7 +12,7 @@ class BudgetApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Budget App',
       theme: ThemeData(
