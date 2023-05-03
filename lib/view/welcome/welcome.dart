e702407af1c10b4baa0b355787851e07281ce536
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-03 21:39:52 +0000

    update welcome UI

--- lib/view/welcome/welcome.dart
+++ lib/view/welcome/welcome.dart
@@ -1,14 +1,17 @@
 import 'package:budget_app_flutter/constants/colors.dart';
+import 'package:budget_app_flutter/helper/calculate_responsiveness.dart';
 import 'package:budget_app_flutter/view/authentication/login.dart';
 import 'package:budget_app_flutter/view/authentication/sign_up.dart';
 import 'package:budget_app_flutter/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final responsiveValues = calculateResponsiveValues(context);
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SafeArea(
@@ -26,22 +29,18 @@ class WelcomeView extends StatelessWidget {
               ),
               Spacer(),
               CustomButton(
+                height: responsiveValues['buttonHeight']!,
                 text: 'Login',
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginView()),
-                  );
+                  Get.offAll(() => LoginView());
                 },
               ),
               SizedBox(height: 10.0),
               CustomButton(
+                height: responsiveValues['buttonHeight']!,
                 text: 'Sign Up',
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpView()),
-                  );
+                  Get.offAll(() => SignUpView());
                 },
               ),
               SizedBox(height: 20.0),
