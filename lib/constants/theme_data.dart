85f33873ce94d982cb8d1885794ee632ee767e9b
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-04 12:55:29 +0000

    create theme data

---
+++ lib/constants/theme_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(),
+);
+
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(),
+);
