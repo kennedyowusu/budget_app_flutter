02c27ad7cbda6fe38efa37807914b6f3d0179033
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-04 08:46:05 +0000

    update the login & signup controllers

--- lib/controller/ signup_controller.dart
+++ lib/controller/ signup_controller.dart
@@ -3,24 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignupController extends GetxController {
-  late TextEditingController nameController;
-  late TextEditingController emailController;
-  late TextEditingController passwordController;
-  late TextEditingController confirmPasswordController;
+  final GlobalKey<FormState> signupFormKey = GlobalKey<FormState>();
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
 
   final RxString name = ''.obs;
   final RxString email = ''.obs;
   final RxString password = ''.obs;
   final RxString confirmPassword = ''.obs;
-
-  @override
-  void onInit() {
-    nameController = TextEditingController();
-    emailController = TextEditingController();
-    passwordController = TextEditingController();
-    confirmPasswordController = TextEditingController();
-    super.onInit();
-  }
 
   @override
   void onClose() {
@@ -47,18 +41,85 @@ class SignupController extends GetxController {
     confirmPassword.value = value;
   }
 
-  void passwordConfirmation() {
-    if (password.value != confirmPassword.value) {
-      ToastWidget.showToast('Passwords do not match');
-      return;
+  bool isFormValid() {
+    return name.isNotEmpty &&
+        email.isNotEmpty &&
+        password.isNotEmpty &&
+        confirmPassword.isNotEmpty &&
+        password.value == confirmPassword.value;
+  }
+
+  String? validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your name';
     }
+    if (value.length < 3) {
+      return 'Name must be at least 3 characters long';
+    }
+    if (value.length > 50) {
+      return 'Name must not exceed 50 characters';
+    }
+    if (!GetUtils.isAlphabetOnly(value)) {
+      return 'Name must contain only alphabets';
+    }
+    if (GetUtils.isNumericOnly(value)) {
+      return 'Name must not contain numbers';
+    }
+    return null;
+  }
+
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    } else if (!GetUtils.isEmail(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters long';
+    } else if (value.length > 50) {
+      return 'Password must not exceed 50 characters';
+    }
+    return null;
+  }
+
+  String? validateConfirmPassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please confirm your password';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters long';
+    } else if (value.length > 50) {
+      return 'Password must not exceed 50 characters';
+    } else if (value != password.value) {
+      return 'Passwords do not match';
+    }
+    return null;
   }
 
   void signUp() {
-    // Implement your sign up logic here
-    // You can access the values using: name.value, email.value, password.value, confirmPassword.value
-    debugPrint(
-      'Name: ${name.value} Email: ${email.value} Password: ${password.value} Confirm Password: ${confirmPassword.value}',
-    );
+    if (!isFormValid()) {
+      ToastWidget.showToast('Please enter valid credentials');
+      return;
+    }
+
+    if (signupFormKey.currentState!.validate() && isFormValid()) {
+      // Implement your sign up logic here
+      debugPrint(
+        'Signing up with name: $name, email: $email, password: $password, confirmPassword: $confirmPassword',
+      );
+
+      // Clear input fields after successful sign up
+      nameController.clear();
+      emailController.clear();
+      passwordController.clear();
+      confirmPasswordController.clear();
+    } else {
+      ToastWidget.showToast('Please enter valid credentials');
+    }
   }
 }
--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,36 +1,74 @@
+import 'package:budget_app_flutter/widgets/custom_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
-  final RxString _username = ''.obs;
-  final RxString _password = ''.obs;
-  late TextEditingController usernameController;
+  final RxString email = ''.obs;
+  final RxString password = ''.obs;
+
+  final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+  late TextEditingController emailController;
   late TextEditingController passwordController;
 
   @override
   void onInit() {
-    usernameController = TextEditingController();
+    emailController = TextEditingController();
     passwordController = TextEditingController();
     super.onInit();
   }
 
   @override
   void onClose() {
-    usernameController.dispose();
+    emailController.dispose();
     passwordController.dispose();
     super.onClose();
   }
 
   void setUsername(String value) {
-    _username.value = value;
+    email.value = value;
   }
 
   void setPassword(String value) {
-    _password.value = value;
+    password.value = value;
+  }
+
+  bool isFormValid() {
+    return email.isNotEmpty && password.isNotEmpty;
   }
 
   void login() {
-    // Implement your login logic here
-    debugPrint('Logging in with username: $_username, password: $_password');
+    if (loginFormKey.currentState!.validate() && isFormValid()) {
+      // Implement your login logic here
+      debugPrint('Logging in with username: $email, password: $password');
+
+      // Clear input fields after successful login
+      emailController.clear();
+      passwordController.clear();
+    } else {
+      ToastWidget.showToast('Please enter valid credentials');
+    }
+  }
+
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    } else if (!GetUtils.isEmail(value)) {
+      return 'Please enter a valid email';
+    }
+
+    // Add additional email validation logic if needed
+
+    return null; // Return null if validation succeeds
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    }
+
+    // Add additional password validation logic if needed
+
+    return null; // Return null if validation succeeds
   }
 }
