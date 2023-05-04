e4f5f630a2e9a38448b70bf646785aee75fe51b1
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-04 13:00:50 +0000

    general update

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,71 @@
+import 'package:budget_app_flutter/controller/theme.dart';
+import 'package:budget_app_flutter/view/home/home.dart';
+import 'package:budget_app_flutter/view/notFound/no_internet.dart';
 import 'package:budget_app_flutter/view/splash/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:connectivity/connectivity.dart';
 
-void main() {
-  runApp(const BudgetApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await GetStorage.init();
+  final GetStorage storage = GetStorage();
+  storage.read("loginResponse");
+  // box.remove('token');
+  runApp(BudgetApp(isUserLoggedIn: storage.hasData('loginResponse')));
 }
 
-class BudgetApp extends StatelessWidget {
-  const BudgetApp({super.key});
+class BudgetApp extends StatefulWidget {
+  const BudgetApp({super.key, required this.isUserLoggedIn});
+  final bool isUserLoggedIn;
+
+  @override
+  State<BudgetApp> createState() => _BudgetAppState();
+}
+
+class _BudgetAppState extends State<BudgetApp> {
+  bool hasInternetConnection = true;
+
+  @override
+  void initState() {
+    super.initState();
+    checkInternetConnection();
+  }
 
   // This widget is the root of your application.
+
+  Future<void> checkInternetConnection() async {
+    final connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        hasInternetConnection = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Budget App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashView(),
+    return GetBuilder<ThemeController>(
+      init: ThemeController(),
+      builder: (controller) {
+        final currentTheme = controller.currentTheme.value;
+        return GetMaterialApp(
+          title: 'Budget App',
+          debugShowCheckedModeBanner: false,
+          transitionDuration: const Duration(milliseconds: 500),
+          defaultTransition: Transition.fade,
+          theme: currentTheme,
+          home: hasInternetConnection
+              ? widget.isUserLoggedIn
+                  ? HomeView()
+                  : SplashView()
+              : NoInternetScreen(
+                  message: 'No internet Connection',
+                ),
+        );
+      },
     );
   }
 }
--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,13 +1,43 @@
+import 'package:budget_app_flutter/helper/calculate_responsiveness.dart';
+import 'package:budget_app_flutter/view/authentication/login.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+  HomeView({Key? key}) : super(key: key);
+
+  final GetStorage box = GetStorage();
 
   @override
   Widget build(BuildContext context) {
+    final responsiveValues = calculateResponsiveValues(context);
+    final TextStyle titleStyle = TextStyle(
+      fontSize: responsiveValues['titleFontSize']!,
+      fontWeight: FontWeight.bold,
+    );
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Text(box.read('loginResponse').toString()),
+            ),
+            SizedBox(height: responsiveValues['verticalSpacing']!),
+            Center(
+              child: Text(box.read('registerResponse').toString()),
+            ),
+            TextButton(
+              onPressed: () {
+                box.remove('loginResponse');
+                box.remove('registerResponse');
+
+                Get.to(() => LoginView());
+              },
+              child: Text("Logout"),
+            )
+          ],
+        ),
       ),
     );
   }
