897adc45a3f1d1e9680d38222358862aa3daf937
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-04 22:33:40 +0000

    create a new file to hold data

---
+++ lib/data/categories.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String, dynamic>> categories = [
+  {
+    "name": "Food",
+    "icon": Icons.fastfood,
+    "date": "04 Aug 2021",
+    "price": "GHS2000.0"
+  },
+  {
+    "name": "Transport",
+    "icon": Icons.directions_car,
+    "date": "07 Aug 2021",
+    "price": "GHS2200.0"
+  },
+  {
+    "name": "Shopping",
+    "icon": Icons.shopping_cart,
+    "date": "09 Aug 2021",
+    "price": "GHS2500.0"
+  },
+  {
+    "name": "Entertainment",
+    "icon": Icons.movie,
+    "date": "12 Aug 2021",
+    "price": "GHS3000.0"
+  },
+  {
+    "name": "Health",
+    "icon": Icons.local_hospital,
+    "date": "15 Aug 2021",
+    "price": "GHS3500.0"
+  },
+  {
+    "name": "Education",
+    "icon": Icons.school,
+    "date": "18 Aug 2021",
+    "price": "GHS4000.0"
+  },
+  {
+    "name": "Others",
+    "icon": Icons.more_horiz,
+    "date": "21 Aug 2021",
+    "price": "GHS4500.0"
+  },
+  {
+    "name": "Others",
+    "icon": Icons.more_horiz,
+    "date": "21 Aug 2021",
+    "price": "GHS4500.0"
+  },
+  {
+    "name": "Others",
+    "icon": Icons.more_horiz,
+    "date": "21 Aug 2021",
+    "price": "GHS4500.0"
+  }
+];
