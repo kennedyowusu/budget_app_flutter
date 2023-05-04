eda8bc1fef0b7232e99f4715dc0da74a0f4db7c1
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-04 18:44:28 +0000

    build unknown screen

---
+++ lib/view/notFound/unknown.dart
@@ -0,0 +1,54 @@
+import 'package:budget_app_flutter/widgets/custom_appbar.dart';
+import 'package:budget_app_flutter/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class UnknownScreen extends StatelessWidget {
+  const UnknownScreen({super.key, required this.message});
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: 'Page Not Found',
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.error_outline,
+              size: 48,
+              color: Colors.red,
+            ),
+            SizedBox(height: 16),
+            Text(
+              message,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'The requested page could not be found.',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 16),
+            CustomButton(
+              text: 'Home',
+              onPressed: () {
+                Get.offAllNamed('/home');
+              },
+              height: 50,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
