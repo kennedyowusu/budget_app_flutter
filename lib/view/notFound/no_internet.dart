ffacb84253017b6e8bd70129dc97d3d35af6f210
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-04 12:58:47 +0000

    create UI for no internet connectivity to display to the user

---
+++ lib/view/notFound/no_internet.dart
@@ -0,0 +1,48 @@
+import 'package:budget_app_flutter/constants/colors.dart';
+import 'package:budget_app_flutter/helper/checkout_internet.dart';
+import 'package:budget_app_flutter/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class NoInternetScreen extends StatelessWidget {
+  final String message;
+
+  const NoInternetScreen({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.wifi_off,
+                  size: 200,
+                  color: Colors.black,
+                ),
+                SizedBox(height: 26),
+                Text(
+                  message.toUpperCase(),
+                  style: titleStyle,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 16),
+                CustomButton(
+                  text: 'Refresh',
+                  onPressed: () {
+                    checkInternetConnectivity(context);
+                  },
+                  height: 50,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
