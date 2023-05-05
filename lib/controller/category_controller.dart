720d5e4a3e02dd3f7ea710cd88f8070da2649bce
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-05 17:56:31 +0000

    create category controller to fetch

---
+++ lib/controller/category_controller.dart
@@ -0,0 +1,32 @@
+import 'package:budget_app_flutter/model/group.dart';
+import 'package:budget_app_flutter/services/category/category.dart';
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+
+class CategoryController extends GetxController {
+  final CategoryService categoryService = CategoryService();
+  final RxList<GroupModel> groupModel = <GroupModel>[].obs;
+  final RxBool isLoading = false.obs;
+
+  @override
+  void onInit() {
+    getUsersCategory();
+    super.onInit();
+  }
+
+  Future<void> getUsersCategory() async {
+    try {
+      isLoading(true);
+      GroupModelResponse groupModelResponse =
+          await categoryService.fetchCategories();
+      groupModel.assignAll(groupModelResponse.data);
+      debugPrint("Group model ${groupModelResponse.data}");
+      debugPrint("Total Groups/Categories for this User: ${groupModel.length}");
+    } catch (e) {
+      isLoading(false);
+      debugPrint("Error fetching a user's category: $e");
+    } finally {
+      isLoading(false);
+    }
+  }
+}
