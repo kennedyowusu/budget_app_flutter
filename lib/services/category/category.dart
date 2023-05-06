11d066d16466ecb7a4f4af7df99a1c82b1356f58
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-06 11:00:12 +0000

    add more print statement and check if data already exist

--- lib/services/category/category.dart
+++ lib/services/category/category.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:budget_app_flutter/model/group.dart';
 import 'package:budget_app_flutter/constants/endpoints.dart';
+import 'package:budget_app_flutter/widgets/custom_toast.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
@@ -26,6 +27,8 @@ class CategoryService {
       if (response.statusCode == 200) {
         final GroupModelResponse groupModelResponse =
             groupModelFromJson(response.body);
+
+        debugPrint("categories: $groupModelResponse");
 
         GetStorage().write("categories", groupModelResponse.data);
         return groupModelResponse;
@@ -52,24 +55,53 @@ class CategoryService {
       };
 
       final token = GetStorage().read('loginResponse');
-      final response = await http.post(
+
+      // Fetch the list of existing categories from the server
+      final response = await http.get(
         uri,
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
           'Authorization': 'Bearer $token'
         },
-        body: json.encode(categoryData),
       );
 
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200 && response.body.isNotEmpty) {
         final GroupModelResponse groupModelResponse =
             groupModelFromJson(response.body);
 
-        GetStorage().write("category", groupModelResponse.data);
-        return groupModelResponse;
+        // Check if the category name already exists
+        if (groupModelResponse.data.any((category) =>
+            category.name.toLowerCase() == categoryName.toLowerCase())) {
+          ToastWidget.showToast(
+            "Category already exists",
+          );
+          throw Exception("Category already exists");
+        }
+
+        // If the category name does not exist, create the category
+        final postResponse = await http.post(
+          uri,
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer $token'
+          },
+          body: json.encode(categoryData),
+        );
+
+        if (postResponse.statusCode == 201 && postResponse.body.isNotEmpty) {
+          final GroupModelResponse newGroupModelResponse =
+              groupModelFromJson(postResponse.body);
+
+          GetStorage().write("category", newGroupModelResponse.data);
+          return newGroupModelResponse;
+        } else {
+          throw Exception(
+              "Failed to create Category ${postResponse.statusCode}");
+        }
       } else {
-        throw Exception("Failed to create Category");
+        throw Exception("Failed to fetch categories ${response.statusCode}");
       }
     } on SocketException {
       throw Exception("No internet connection");
